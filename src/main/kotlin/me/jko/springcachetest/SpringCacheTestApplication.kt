71f7be47a7ccefe6c7f92b494b3476f3b997d060
junhee-ko <junheee.ko@gmail.com>
2025-07-06 14:36:40 +0900

    add @EnableCaching in SpringCacheTestApplication

--- src/main/kotlin/me/jko/springcachetest/SpringCacheTestApplication.kt
+++ src/main/kotlin/me/jko/springcachetest/SpringCacheTestApplication.kt
@@ -2,8 +2,10 @@ package me.jko.springcachetest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class SpringCacheTestApplication
 
 fun main(args: Array<String>) {
