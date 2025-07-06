a748b2b369c26a36341791ae71892b096eeb05c4
junhee-ko <junheee.ko@gmail.com>
2025-07-06 14:35:14 +0900

    first commit

---
+++ src/test/kotlin/me/jko/springcachetest/SpringCacheTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jko.springcachetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringCacheTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
